6417ac727968f2d50d524690ad8366ccc1430bd9
Ronald Tomaz <ronaldtmprofile@gmail.com>
2021-11-04 20:49:30 -0300

    :rocket: version 1.0 complete

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const links = document.querySelectorAll("nav ul li a span");
 const menuIcon = document.querySelector(".menu img");
-const logo = document.querySelector(".logo img");
-const inputForm = document.querySelector("input");
 
 const sidebar = document.querySelector(".sidebar");
-const avatar = document.querySelector("footer .avatar");
+const hideSidebarClass = "hide-sidebar";
 
 menuIcon.addEventListener("click", () => {
+  sidebar.classList.toggle(hideSidebarClass);
+  hideLinks();
+});
+
+function hideLinks() {
   links.forEach(link => {
     link.classList.toggle("hide");
   });
-
-  sidebar.style.width = "78px";
-
-  inputForm.classList.toggle("hide");
-  logo.classList.toggle("hide");
-  avatar.classList.toggle("hide");
-});
+}
